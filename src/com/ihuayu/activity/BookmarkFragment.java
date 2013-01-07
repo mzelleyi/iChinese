2b504c97cbc7b1775a32c606878b7cf3f151f371
kesenhoo <kesenhoo_hu@htc.com>
2013-01-07 20:09:49 +0800

    Add Bookmark Edit Mode

--- src/com/ihuayu/activity/BookmarkFragment.java
+++ src/com/ihuayu/activity/BookmarkFragment.java
@@ -15,6 +15,7 @@ import com.ihuayu.R;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -31,7 +32,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -42,8 +45,12 @@ import android.widget.TextView;
  */
 public class BookmarkFragment extends Fragment {
 
-	private static final String TAG = "iHuayu:BookmarkFragment";
-	private static FragmentActivity parentActivity = null;
+	private static final String		TAG						= "iHuayu:BookmarkFragment";
+	private static FragmentActivity	mParentActivity			= null;
+	private static Resources		mRes					= null;
+	private BookmarkListFragment	mBookmarkListFragment	= null;
+	private static final int		VIEW_TYPE_NORMAL		= 0;
+	private static final int		VIEW_TYPE_DIVIDER		= 1;
 
     /**
      * Create a new instance of SearchFragment
@@ -51,11 +58,6 @@ public class BookmarkFragment extends Fragment {
     static BookmarkFragment newInstance( ) {
     	Log.d(TAG, "[newInstance] + Begin");
     	BookmarkFragment fragment = new BookmarkFragment();
-
-        // Supply num input as an argument.
-        //Bundle args = new Bundle();
-        //args.putInt("num", num);
-        //f.setArguments(args);
         return fragment;
     }
     
@@ -64,7 +66,6 @@ public class BookmarkFragment extends Fragment {
 		Log.d(TAG, "[onCreate] + Begin");
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		//mNum = getArguments() != null ? getArguments().getInt("num") : 1;
 	}
 	
     /**
@@ -83,24 +84,42 @@ public class BookmarkFragment extends Fragment {
 		Log.d(TAG, "[onViewCreated] + Begin");
 		// TODO Auto-generated method stub
 		super.onViewCreated(view, savedInstanceState);
-		parentActivity = this.getActivity();
+		mParentActivity = this.getActivity();
+		mRes = mParentActivity.getResources();
 		
-		FragmentManager fm = parentActivity.getSupportFragmentManager();
+		FragmentManager fm = mParentActivity.getSupportFragmentManager();
 		FragmentTransaction ft = fm.beginTransaction();
-		BookmarkListFragment list = (BookmarkListFragment) fm.findFragmentById(R.id.fragment_bookmark_listview);
+		mBookmarkListFragment = (BookmarkListFragment) fm.findFragmentById(R.id.fragment_bookmark_listview);
+		
+		//Set Edit Button On Click Listener
+		final Button editBtn = (Button)mParentActivity.findViewById(R.id.bookmark_titlebar_btn_edit);
+		editBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (editBtn.getText().toString().equalsIgnoreCase(
+						mRes.getString(R.string.title_bar_btn_edit))) {
+					mBookmarkListFragment.enterEditMode();
+					editBtn.setText(R.string.title_bar_btn_done);
+				} else {
+					mBookmarkListFragment.leaveEditMode();
+					editBtn.setText(R.string.title_bar_btn_edit);
+				}
+			}
+		});
+		
         // Create the list fragment and add it as our sole content.
-        if (list == null) {
+        if (mBookmarkListFragment == null) {
         	Log.d(TAG, "[onViewCreated] new BookmarkListFragment, do add");
-        	list = new BookmarkListFragment();
-			ft.add(R.id.fragment_bookmark_listview, list);
+        	mBookmarkListFragment = new BookmarkListFragment();
+			ft.add(R.id.fragment_bookmark_listview, mBookmarkListFragment);
 			ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
 			//ft.addToBackStack(null);
 			ft.commit();
         } else {
         	Log.d(TAG, "[onViewCreated] used BookmarkListFragment, do replace");
-        	list = new BookmarkListFragment();
+        	mBookmarkListFragment = new BookmarkListFragment();
 			ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-			ft.replace(R.id.fragment_bookmark_listview, list);
+			ft.replace(R.id.fragment_bookmark_listview, mBookmarkListFragment);
 			//ft.addToBackStack(null);
 			ft.commit();
         }
@@ -150,6 +169,7 @@ public class BookmarkFragment extends Fragment {
 			
 		}
 	
+		@Override
 		public Loader<List<BookmarkEntry>> onCreateLoader(int id, Bundle args) {
 			Log.d(TAG, "[BookmarkListFragment][onCreateLoader] + Begin");
 			// This is called when a new Loader needs to be created. This
@@ -157,6 +177,7 @@ public class BookmarkFragment extends Fragment {
 			return new BookmarkListLoader(this.getActivity());
 		}
 	
+		@Override
 		public void onLoadFinished(Loader<List<BookmarkEntry>> loader, List<BookmarkEntry> data) {
 			Log.d(TAG, "[BookmarkListFragment][onLoadFinished] + Begin");
 			// Set the new data in the adapter.
@@ -170,10 +191,38 @@ public class BookmarkFragment extends Fragment {
 			}
 		}
 	
+		@Override
 		public void onLoaderReset(Loader<List<BookmarkEntry>> loader) {
 			Log.d(TAG, "[BookmarkListFragment][onLoaderReset] + Begin");
 			// Clear the data in the adapter.
 			mAdapter.setData(null);
+		}
+		
+		public void enterEditMode() {
+			Log.d(TAG, "[BookmarkListFragment][enterEditMode] + Begin");
+			mAdapter.bEditMode = true;
+			mAdapter.notifyDataSetChanged();
+			Log.d(TAG, "[BookmarkListFragment][enterEditMode] + End");
+		}
+		
+		public void leaveEditMode() {
+			Log.d(TAG, "[BookmarkListFragment][leaveEditMode] + Begin");
+			mAdapter.bEditMode = false;
+			
+			int count = mAdapter.getCount();
+			Log.d(TAG, "[BookmarkListFragment] Adapter.getCount = "+count);
+			for (int i = 0 ; i < count; i++) {
+				Log.d(TAG, "[BookmarkListFragment] i = "+i);
+				BookmarkEntry entry = mAdapter.getItem(i);
+				if (entry.mNeedDelete) {
+					Log.d(TAG, "[BookmarkListFragment] remove item i ="+i);
+					count--;
+					Log.d(TAG, "[BookmarkListFragment] now count ="+count);
+					mAdapter.remove(entry);
+				}
+			}
+			mAdapter.notifyDataSetChanged();
+			Log.d(TAG, "[BookmarkListFragment][leaveEditMode] + End");
 		}
 	}
 	
@@ -248,6 +297,7 @@ public class BookmarkFragment extends Fragment {
 	    private final File mApkFile;
 	    private String mLabel;
 	    private boolean misDivider = false;
+	    private boolean mNeedDelete = false;
 	    private Drawable mIcon;
 	    private boolean mMounted;
 	}
@@ -409,6 +459,7 @@ public class BookmarkFragment extends Fragment {
 
 	public static class BookmarkListAdapter extends ArrayAdapter<BookmarkEntry> {
 	    private final LayoutInflater mInflater;
+	    private boolean bEditMode = false;
 	    
 	    public BookmarkListAdapter(Context context) {
 	        //super(context, android.R.layout.simple_list_item_2);
@@ -463,75 +514,147 @@ public class BookmarkFragment extends Fragment {
 	        Log.d(TAG, "[BookmarkListAdapter][setData] + End");
 	    }
 	    
+	    @Override
+		public int getItemViewType(int position)
+		{
+			// TODO Auto-generated method stub
+	    	BookmarkEntry item = this.getItem(position);
+	    	if (item.misDivider) {
+	    		return VIEW_TYPE_DIVIDER;
+	    	} else {
+	    		return VIEW_TYPE_NORMAL;
+	    	}
+			//return super.getItemViewType(position);
+		}
+	    
+		@Override
+		public int getViewTypeCount()
+		{
+			// TODO Auto-generated method stub
+			return 2;
+			//return super.getViewTypeCount();
+		}
+		
 		class ViewHolder
 		{
-			public ImageView	listImageIcon	= null;
-			public TextView		listTextView1	= null;
-			public TextView		listTextView2	= null;
-			public TextView		listTextView3	= null;
-			public TextView     listTextType    = null;
+			public ImageView    listImageDelete     = null;
+			public ImageView	listImageSpearker   = null;
+			public TextView		listTextView1		= null;
+			public TextView		listTextView2		= null;
+			public TextView		listTextView3		= null;
+			public TextView		listTextCategory	= null;
 		}
 
-	    /**
+		/**
 		 * Populate new items in the list.
 		 */
 	    @Override 
 	    public View getView(int position, View convertView, ViewGroup parent) {
-	    	Log.d(TAG, "[BookmarkListAdapter][getView] + pos = "+position);
+	    	Log.d(TAG, "[BookmarkListAdapter][getView] + pos = "+position+",EditMode = "+bEditMode);
 	        //View view = null;
-			//ViewHolder holder = null;
+			ViewHolder holder = null;
 			TextView 	separatorText = null;
-			ImageView	listImageIcon	= null;
-			TextView	listTextView1	= null;
-			TextView	listTextView2	= null;
-			TextView	listTextView3	= null;
-			TextView    listTextType    = null;
+//			ImageView	listImageIcon	= null;
+//			TextView	listTextView1	= null;
+//			TextView	listTextView2	= null;
+//			TextView	listTextView3	= null;
+//			TextView    listTextType    = null;
 			final BookmarkEntry item = this.getItem(position);
-			Log.d(TAG, "[BookmarkListAdapter][getView] isDivider = "+item.misDivider+"; label = "+item.getLabel());
+			Log.d(TAG, "isDivider = "+item.misDivider+"; label = "+item.getLabel());
 			if (item != null) {				
 				if (convertView == null) {
-					Log.d(TAG, "convertView == null");
-					if (item.misDivider) {
-						Log.d(TAG, "inflate then find bookmark_list_separate");
+					if (this.getItemViewType(position) == VIEW_TYPE_DIVIDER) {
+						Log.d(TAG, "convertView == null, inflate -> find bookmark_list_separate");
 						convertView = mInflater.inflate(R.layout.bookmark_list_separate, parent, false);
 						separatorText = (TextView) convertView.findViewById(R.id.bookmark_listitem_separate_text);
 					} else {
-						Log.d(TAG, "inflate then find bookmark_list_item");
+						Log.d(TAG, "convertView == null, inflate -> find bookmark_list_item");
+						holder = new ViewHolder();
 						convertView = mInflater.inflate(R.layout.bookmark_list_item, parent, false);
-						listImageIcon = (ImageView) convertView.findViewById(R.id.bookmark_listitem_icon_speaker);
-						listTextView1 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_first_line);
-						listTextView2 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_second_line);
-						listTextView3 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_third_line);
-						listTextType = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_type);
+						if (bEditMode) {
+//							ViewStub stub = (ViewStub) convertView.findViewById(R.id.bookmark_listitem_left_viewStub);
+//							View inflated = stub.inflate();
+						    holder.listImageDelete  = (ImageView) convertView.findViewById(R.id.bookmark_listview_viewstub_img);
+						} else {
+							ViewStub stub = (ViewStub) convertView.findViewById(R.id.bookmark_listitem_left_viewStub);
+							if (stub != null) {
+								stub.setVisibility(View.GONE);
+								holder.listImageDelete = null;
+							}
+						}
+						
+						holder.listImageSpearker = (ImageView) convertView.findViewById(R.id.bookmark_listitem_icon_speaker);
+						holder.listTextView1 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_first_line);
+						holder.listTextView2 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_second_line);
+						holder.listTextView3 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_third_line);
+						holder.listTextCategory = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_type);
+						convertView.setTag(holder);
 					}
 				} else {
-					Log.d(TAG, "convertView £¡= null");
-					if (item.misDivider) {
-						Log.d(TAG, "inflate and find separatorText");
-						convertView = mInflater.inflate(R.layout.bookmark_list_separate, parent, false);
+					if (this.getItemViewType(position) == VIEW_TYPE_DIVIDER) {
+						Log.d(TAG, "convertView != null, find separatorText");
+						//convertView = mInflater.inflate(R.layout.bookmark_list_separate, parent, false);
 						separatorText = (TextView) convertView.findViewById(R.id.bookmark_listitem_separate_text);
 					} else {
-						Log.d(TAG, "inflate and find bookmark_list_item");
-						convertView = mInflater.inflate(R.layout.bookmark_list_item, parent, false);
-						listImageIcon = (ImageView) convertView.findViewById(R.id.bookmark_listitem_icon_speaker);
-						listTextView1 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_first_line);
-						listTextView2 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_second_line);
-						listTextView3 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_third_line);
-						listTextType = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_type);
+						Log.d(TAG, "convertView != null, find bookmark_list_item");
+						//convertView = mInflater.inflate(R.layout.bookmark_list_item, parent, false);
+//						listImageIcon = (ImageView) convertView.findViewById(R.id.bookmark_listitem_icon_speaker);
+//						listTextView1 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_first_line);
+//						listTextView2 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_second_line);
+//						listTextView3 = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_third_line);
+//						listTextType = (TextView) convertView.findViewById(R.id.bookmark_listitem_text_type);
+						holder = (ViewHolder) convertView.getTag();
 					}
 				}
 				
 				if (null != convertView) {
-					if (item.misDivider) {
+					if (this.getItemViewType(position) == 1) {
 						if (separatorText != null) {
 							Log.d(TAG, "set Separator Text");
 							separatorText.setText(String.valueOf(item.getLabel().charAt(0)));
 						}
 					} else {
 						Log.d(TAG, "set ListItem Data");
-						if (listImageIcon != null) {
-							listImageIcon.setImageDrawable(item.getIcon());
-							listImageIcon.setOnClickListener(new View.OnClickListener()	{
+						
+						if (bEditMode) {
+							ViewStub stub = (ViewStub) convertView.findViewById(R.id.bookmark_listitem_left_viewStub);
+							if (holder.listImageDelete == null) {
+								View inflated = stub.inflate();
+								holder.listImageDelete = (ImageView) inflated.findViewById(R.id.bookmark_listview_viewstub_img);
+							}
+							final ImageView deleteImg = holder.listImageDelete;
+							
+							if (item.mNeedDelete) {
+								deleteImg.setImageResource(R.drawable.btn_clear_on);
+							} else {
+								deleteImg.setImageResource(R.drawable.btn_clear_rest);
+							}
+							
+							deleteImg.setOnClickListener(new View.OnClickListener() {
+								@Override
+								public void onClick(View arg0)
+								{
+									// TODO Auto-generated method stub
+									if (item.mNeedDelete) {
+										deleteImg.setImageResource(R.drawable.btn_clear_rest);
+										item.mNeedDelete = false;
+									} else {
+										deleteImg.setImageResource(R.drawable.btn_clear_on);
+										item.mNeedDelete = true;
+									}
+								}
+							});
+						} else {
+							ViewStub stub = (ViewStub) convertView.findViewById(R.id.bookmark_listitem_left_viewStub);
+							if (stub != null) {
+								stub.setVisibility(View.GONE);
+								holder.listImageDelete = null;
+							}
+						}
+	
+						if (holder.listImageSpearker != null) {
+							holder.listImageSpearker.setImageDrawable(item.getIcon());
+							holder.listImageSpearker.setOnClickListener(new View.OnClickListener()	{
 								@Override
 								public void onClick(View v)
 								{
@@ -539,17 +662,17 @@ public class BookmarkFragment extends Fragment {
 								}
 							});
 						}
-						if (listTextView1 != null) {
-							listTextView1.setText(item.getLabel());
+						if (holder.listTextView1 != null) {
+							holder.listTextView1.setText(item.getLabel());
 						}
-						if (listTextView2 != null) {
-							listTextView2.setText(item.getLabel());
+						if (holder.listTextView2 != null) {
+							holder.listTextView2.setText(item.getLabel());
 						}
-						if (listTextView3 != null) {
-							listTextView3.setText(item.getLabel());
+						if (holder.listTextView3 != null) {
+							holder.listTextView3.setText(item.getLabel());
 						}
-						if (listTextType != null) {
-							listTextType.setText("Test");
+						if (holder.listTextCategory != null) {
+							holder.listTextCategory.setText("Category");
 						}
 					}
 				}
